83239bf2937a939bbfd860354c0208526892bded
Dillon Forestell <dillons-mac@Dillons-MacBook-Pro.local>
2019-07-30 15:25:32 -0500

    add lodash library and use the shuffle function to shuffle our answers everytime a question is rendered

--- src/components/QuestionBox.vue
+++ src/components/QuestionBox.vue
@@ -8,7 +8,12 @@
       <hr class="my-4">
 
       <b-list-group>
-        <b-list-group-item v-for="(answer, index) in answers" :key="index">
+        <b-list-group-item
+          v-for="(answer, index) in answers"
+          :key="index"
+          @click="selectAnswer(index)"
+          :class="[selectedIndex === index ? 'selected' : '']"
+        >
           {{ answer }}
         </b-list-group-item>
       </b-list-group>
@@ -20,16 +25,45 @@
 </template>
 
 <script>
+  import _ from 'lodash';
+
   export default {
     props: {
       currentQuestion: Object,
       nextQuestion: Function,
     },
+
+    data(){
+      return{
+        selectedIndex: null,
+        shuffledAnswers: []
+      }
+    },
+
     computed: {
       answers(){
         let answers = [...this.currentQuestion.incorrect_answers]
         answers.push(this.currentQuestion.correct_answer)
         return answers
+      }
+    },
+
+    watch: {
+      currentQuestion: {
+        immediate: true,
+        handler(){
+          this.selectedIndex = null
+          this.shuffleAnswers();
+        }
+      }
+    },
+    methods: {
+      selectAnswer(index){
+        this.selectedIndex = index
+      },
+      shuffleAnswers(){
+        let answers = [...this.currentQuestion.incorrect_answers, this.currentQuestion.correct_answer]
+        this.shuffledAnswers = _.shuffle(answers)
       }
     }
   }
@@ -41,8 +75,29 @@
     margin-bottom: 15px;
   }
 
+  .list-group-item:hover{
+    background: #EEE;
+    cursor: pointer;
+  }
+
   .btn{
     margin: 0 8px;
+  }
+
+  .selected {
+    background-color: lightblue;
+  }
+
+  .selected:hover{
+    background: lightblue;
+  }
+
+  .correct {
+    background-color: lightgreen;
+  }
+
+  .incorrect{
+    background-color: red;
   }
 
 </style>
