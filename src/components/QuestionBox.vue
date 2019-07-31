b74ccd025eec77426251757ae99cbac897657211
Dillon Forestell <dillons-mac@Dillons-MacBook-Pro.local>
2019-07-31 11:34:11 -0500

    pull logic out of the template and move it into the methods object of the script

--- src/components/QuestionBox.vue
+++ src/components/QuestionBox.vue
@@ -12,7 +12,7 @@
           v-for="(answer, index) in shuffledAnswers"
           :key="index"
           @click="selectAnswer(index)"
-          :class="[selectedIndex === index ? 'selected' : '']"
+          :class="answerClass(index)"
         >
           {{ answer }}
         </b-list-group-item>
@@ -84,6 +84,20 @@
         let answers = [...this.currentQuestion.incorrect_answers, this.currentQuestion.correct_answer]
         this.shuffledAnswers = _.shuffle(answers)
         this.correctIndex = this.shuffledAnswers.indexOf(this.currentQuestion.correct_answer)
+      },
+      answerClass(index){
+        let answerClass = ''
+
+        if (!this.answered && this.selectedIndex === index){
+          answerClass = 'selected'
+        }
+        else if (this.answered && this.correctIndex === index) {
+          answerClass = 'correct'
+        }
+        else if (this.answered && this.selectedIndex === index && this.correctIndex !== index) {
+          answerClass = 'incorrect'
+        }
+        return answerClass
       }
     }
   }
